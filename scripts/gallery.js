d07216e834c234154e1d1f4145a1d1c30f1a6bd1
Pikurrot <pikurrot@gmail.com>
2024-11-24 17:15:45 +0100

    Update script to load images from aws

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,18 +1,20 @@
 const galleryContainer = document.getElementById('gallery-container');
 
-// Base URLs for images
-const localServerBase = "http://172.22.125.12:8080/mine_server_gallery/";
-const imageListUrl = "http://172.22.125.12:8080/list_images.php";
+// S3 base URL for your bucket
+const cloudBase = "https://minecraft-server-gallery.s3.amazonaws.com/";
 
-// Fetch the list of images from the PHP endpoint
-fetch(imageListUrl)
-    .then(response => response.json())
-    .then(images => {
-        images.forEach(img => {
-            const imgElement = document.createElement('img');
-            imgElement.src = `${localServerBase}${img}`;
-            imgElement.alt = `Gallery Image - ${img}`;
-            galleryContainer.appendChild(imgElement);
-        });
-    })
-    .catch(err => console.error('Failed to fetch image list:', err));
+// List of images (you can maintain a list, or generate a file in S3 to list them dynamically)
+const images = [
+    "2024-09-14_13.22.06.png",
+    "2024-09-14_13.23.44.png",
+    "2024-10-30_21.06.44.png",
+    "2024-11-01_19.37.00.png"
+];
+
+// Load images into gallery
+images.forEach(img => {
+    const imgElement = document.createElement('img');
+    imgElement.src = `${cloudBase}${img}`;
+    imgElement.alt = `Gallery Image - ${img}`;
+    galleryContainer.appendChild(imgElement);
+});
